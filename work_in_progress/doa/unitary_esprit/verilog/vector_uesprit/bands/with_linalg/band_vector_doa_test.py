64a888d6f57a7791ca9e6f574621de3462b80a56
seba <junkseba@gmail.com>
2022-04-11 13:03:49 -0400

    modify band_doa tb to allow plot the predicted doa

--- work_in_progress/doa/unitary_esprit/verilog/vector_uesprit/bands/with_linalg/band_vector_doa_test.py
+++ work_in_progress/doa/unitary_esprit/verilog/vector_uesprit/bands/with_linalg/band_vector_doa_test.py
@@ -2,6 +2,7 @@ import cocotb, sys
 from scipy.fftpack import fft
 sys.path.append('../../../../')
 import numpy as np
+import matplotlib.pyplot as plt
 from itertools import cycle
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles, FallingEdge
@@ -12,6 +13,150 @@ from two_comp import two_comp_pack, two_comp_unpack
 ### Author: Sebastian Jorquera
 ###
 
+
+@cocotb.test()
+async def point_doa_no_la(dut, iters=100, acc_len=10, vec_len=64,bands=4,
+        din_width=16, din_pt=14, dout_width=16, dout_pt=8, corr_shift=0,
+        corr_width=16, corr_pt=8, corr_thresh=1,cont=1, burst_len=10, 
+        thresh=0.2, collect_phases=1, print_all=0):
+    ##hyper params for the data generation
+    freqs = [3,  33, 54]
+    phases = [-10, -33, -90]
+    amps = [0.02, 0.2, 0.05]
+
+    iters_doa = 65*2
+
+    thresh = thresh*acc_len
+    
+    ##
+    clk = Clock(dut.clk, 10, units='ns')
+    cocotb.fork(clk.start())
+    np.random.seed(29)
+
+    #setup the dut 
+    dut.din1_re0.value = 0
+    dut.din1_im0.value = 0
+    dut.din2_re0.value = 0
+    dut.din2_im0.value = 0
+
+    dut.din1_re1.value = 0
+    dut.din1_im1.value = 0
+    dut.din2_re1.value = 0
+    dut.din2_im1.value = 0
+
+    dut.din1_re2.value = 0
+    dut.din1_im2.value = 0
+    dut.din2_re2.value = 0
+    dut.din2_im2.value = 0
+
+    dut.din1_re3.value = 0
+    dut.din1_im3.value = 0
+    dut.din2_re3.value = 0
+    dut.din2_im3.value = 0
+
+    dut.din_valid.value =0
+    dut.new_acc.value =0
+    
+    await ClockCycles(dut.clk, 5)
+
+    ##generate input data
+    generator = gen_data(dft_len=vec_len,iters=iters*acc_len, freqs=freqs, phases=phases, 
+            amplitude=amps, noise_std=10**-3)
+
+    dat0, dat1 = (generator.antenna0, generator.antenna1)
+    norm = np.max(np.array([dat0.real, dat0.imag, dat1.real, dat1.imag]))
+    dat0 = dat0/norm
+    dat1 = dat1/norm
+   
+    #plot the input spectra
+    fig=plt.figure();   ax1 = fig.add_subplot(121); ax2=fig.add_subplot(122)
+    ax1.plot(20*np.log10(np.abs(dat0[0,:])))
+    ax2.plot(20*np.log10(np.abs(dat1[0,:])))
+    ax1.set_title('Antenna 0')
+    ax2.set_title('Antenna 1')
+    ax1.grid()
+    ax2.grid()
+    plt.savefig('in_spect.png')
+    plt.close()
+
+    ##gold results
+    r11,r22,r12 = uesprit_matrix(dat0.T, dat1.T, acc_len)
+
+    r11 = np.sum(r11.reshape([bands, vec_len//bands, iters]), axis=1)
+    r22 = np.sum(r22.reshape([bands, vec_len//bands, iters]), axis=1)
+    r12 = np.sum(r12.reshape([bands, vec_len//bands, iters]), axis=1)
+    
+    r11 = r11/2.**corr_shift
+    r12 = r12/2.**corr_shift
+    r22 = r22/2.**corr_shift
+
+    gold_corr = [r11.T.flatten(),r22.T.flatten(), r12.T.flatten()]
+
+    l1,l2,eig1,eig2,frac = uesprit_eigen(r11.T.flatten(),r22.T.flatten(),r12.T.flatten())
+    gold = [l1,l2,eig1,eig2,frac]
+
+    ##convert data into binary
+    din0_re = two_comp_pack(dat0.flatten().real, din_width, din_pt)
+    din0_im = two_comp_pack(dat0.flatten().imag, din_width, din_pt)
+    din1_re = two_comp_pack(dat1.flatten().real, din_width, din_pt)
+    din1_im = two_comp_pack(dat1.flatten().imag, din_width, din_pt)
+
+    data = [din0_re+1j*din0_im, din1_re+1j*din1_im]
+    
+    ##like we subsample in the frequencies
+    freq = np.array(freqs)//(vec_len/4)
+    
+    
+    ##start simulation
+
+    cocotb.fork(read_data(dut, gold, bands, dout_width, dout_pt, freq,
+                thresh, print_all=print_all))
+    if(collect_phases): 
+        cocotb.fork(write_data(dut,data, acc_len, vec_len//bands, cont, burst_len))
+        gold_phases,rtl_phases = await collect_doa(dut, gold,vec_len, dout_width, dout_pt, freq.astype(int), iters_doa)
+        fig = plt.figure()
+        ax_shape = get_fig_shape(len(freq))
+         
+        colors = ['b','g','r','c','m','y','k']
+        for i in range(len(freq)):
+            ax = fig.add_subplot(ax_shape[0],ax_shape[1],i+1)
+            ax_lim = [-phases[i]-40, -phases[i]+40]
+            ax.set_title('Band %i  doa: %.3f' %(freq[i], -phases[i]))
+            ax.plot(gold_phases[str(int(freq[i]))], colors[0]+'x', label='gold doa')
+            ax.plot(rtl_phases[str(int(freq[i]))], colors[1]+'.', label='rtl doa')
+            plt.axhline(np.median(rtl_phases[str(int(freq[i]))]),0, 
+                    len(rtl_phases[str(int(freq[i]))]), color=colors[2],
+                    linestyle='--', label='rtl median')
+            plt.axhline(np.median(gold_phases[str(int(freq[i]))]),0, 
+                    len(rtl_phases[str(int(freq[i]))]), color=colors[3],
+                    linestyle='-.', label='gold median')
+            ax.set_ylim(ax_lim)
+            ax.grid()
+        line_label = [ax.get_legend_handles_labels()]
+        lines, labels = [sum(lol,[]) for lol in zip(*line_label)]
+        plt.tight_layout()
+        fig.legend(lines, labels, bbox_to_anchor=(1.04,0))
+        plt.savefig('doa.png',bbox_inches = "tight")
+        plt.close()
+    else:
+        await (write_data(dut,data, acc_len, vec_len//bands, cont, burst_len))
+       
+
+def get_fig_shape(n_data):
+    if(n_data<3):
+        return (1,n_data)
+    elif(n_data<5):
+        return (2,2)
+    elif(n_data<7):
+        return (2,3)
+    elif(n_data<10):
+        return (3,3)
+    else:
+        return (4,4)
+
+
+    
+    
 class gen_data():
     def __init__(self, dft_len=64,iters=10, freqs=[10,30], phases=[20, 50], 
             amplitude=[0.2,1 ], noise_std=10**-3):
@@ -78,103 +223,6 @@ def uesprit_eigen(r11,r22,r12):
     eigfrac = r12
     return [lamb1, lamb2,eigvec1,eigvec2,eigfrac]
 
-
-@cocotb.test()
-async def point_doa_no_la(dut, iters=100, acc_len=10, vec_len=64,bands=4,
-        din_width=16, din_pt=14, dout_width=16, dout_pt=8, corr_shift=0,
-        corr_width=16, corr_pt=8, corr_thresh=1,
-        cont=1, burst_len=10, thresh=0.2):
-    ##hyper params for the data generation
-    freqs = [2, 33]
-    phases = [70, 33]
-    amps = [0.2, 0.2]
-
-    thresh = thresh*acc_len
-    
-    ##
-    clk = Clock(dut.clk, 10, units='ns')
-    cocotb.fork(clk.start())
-    np.random.seed(19)
-
-    #setup the dut 
-    dut.din1_re0.value = 0
-    dut.din1_im0.value = 0
-    dut.din2_re0.value = 0
-    dut.din2_im0.value = 0
-
-    dut.din1_re1.value = 0
-    dut.din1_im1.value = 0
-    dut.din2_re1.value = 0
-    dut.din2_im1.value = 0
-
-    dut.din1_re2.value = 0
-    dut.din1_im2.value = 0
-    dut.din2_re2.value = 0
-    dut.din2_im2.value = 0
-
-    dut.din1_re3.value = 0
-    dut.din1_im3.value = 0
-    dut.din2_re3.value = 0
-    dut.din2_im3.value = 0
-
-    dut.din_valid.value =0
-    dut.new_acc.value =0
-    
-    await ClockCycles(dut.clk, 5)
-
-    ##generate input data
-    generator = gen_data(dft_len=vec_len,iters=iters*acc_len, freqs=freqs, phases=phases, 
-            amplitude=amps, noise_std=10**-3)
-
-    dat0, dat1 = (generator.antenna0, generator.antenna1)
-    norm = np.max(np.array([dat0.real, dat0.imag, dat1.real, dat1.imag]))
-    dat0 = dat0/norm
-    dat1 = dat1/norm
-    
-    ##gold results
-    r11,r22,r12 = uesprit_matrix(dat0.T, dat1.T, acc_len)
-
-    r11 = np.sum(r11.reshape([bands, vec_len//bands, iters]), axis=1)
-    r22 = np.sum(r22.reshape([bands, vec_len//bands, iters]), axis=1)
-    r12 = np.sum(r12.reshape([bands, vec_len//bands, iters]), axis=1)
-    
-    r11 = r11/2.**corr_shift
-    r12 = r12/2.**corr_shift
-    r22 = r22/2.**corr_shift
-
-    #print(r11.T.flatten()[:8])
-    #print(r12.T.flatten()[:8])
-    #print(r22.T.flatten()[:8])
-
-    gold_corr = [r11.T.flatten(),r22.T.flatten(), r12.T.flatten()]
-
-
-    l1,l2,eig1,eig2,frac = uesprit_eigen(r11.T.flatten(),r22.T.flatten(),r12.T.flatten())
-    gold = [l1,l2,eig1,eig2,frac]
-
-    ##convert data into binary
-    din0_re = two_comp_pack(dat0.flatten().real, din_width, din_pt)
-    din0_im = two_comp_pack(dat0.flatten().imag, din_width, din_pt)
-    din1_re = two_comp_pack(dat1.flatten().real, din_width, din_pt)
-    din1_im = two_comp_pack(dat1.flatten().imag, din_width, din_pt)
-
-    data = [din0_re+1j*din0_im, din1_re+1j*din1_im]
-    
-    ##like we subsample in the frequencies
-    freq = np.array(freqs)//bands
-    
-    
-    ##start simulation
-    print(dat0.flatten()[:10])
-    print(dat1.flatten()[:10])
-
-    #check the output of the correlator, for debuging
-    #cocotb.fork(check_correlator_output(dut, gold_corr,vec_len//bands, corr_width, corr_pt, 
-    #    corr_thresh))
-    #check global output
-    cocotb.fork(read_data(dut, gold, bands, dout_width, dout_pt, freqs,
-                thresh, print_all=1))
-    await write_data(dut,data, acc_len, vec_len//bands, cont, burst_len)
     
 
 
@@ -218,7 +266,18 @@ async def write_data(dut, data, acc_len,vec_len, cont, burst_len):
         return 1
 
 
-async def read_data(dut, gold, vec_len, dout_width, dout_pt, freqs, thresh, print_all=0):
+async def read_data(dut, gold, vec_len, dout_width, dout_pt, freqs, thresh, print_all=0,
+        collect_doa=None):
+    """ gold        :   floating point values
+        vec_len     :   number of fft channels
+        dout_width  :   output bitwidth
+        dout_pt     :   output binary point
+        freqs       :   list with the frequencies of the soi
+        thresh      :   threshold for determine an error (gold-rtl_values)
+        print_all   :   print all valid output (even if there is not signal in that band)
+        collect_doa :   to collect the rtl and gold DoA, put a global variable  
+                        to be able to get the value if you want to kill the process
+    """
     count = 0
     while(count < vec_len):
         valid = int(dut.dout_valid.value)
@@ -251,7 +310,6 @@ async def read_data(dut, gold, vec_len, dout_width, dout_pt, freqs, thresh, prin
                 gold_phase = np.rad2deg(np.arctan2(gold[2][count].real, gold[4][count].real)*2)
                 rtl_phase = np.rad2deg(np.arctan2(float(outs[2]), float(outs[4]))*2)
                 print("phase \t gold: %.4f \t rtl: %.4f" %(gold_phase, rtl_phase))
-
             assert (np.abs(gold[0][count]-outs[0])<thresh), "l1 error"
             assert (np.abs(gold[1][count]-outs[1])<thresh), "l2 error"
             assert (np.abs(gold[2][count]-outs[2])<thresh), "eig1 error"
@@ -259,6 +317,45 @@ async def read_data(dut, gold, vec_len, dout_width, dout_pt, freqs, thresh, prin
             assert (np.abs(gold[4][count]-outs[4])<thresh), "eig frac error"
             count += 1
         await ClockCycles(dut.clk, 1)
+
+            
+async def collect_doa(dut, gold,vec_len, dout_width, dout_pt, bands, iters):
+    count = 0
+    while(count < vec_len):
+        valid = int(dut.dout_valid.value)
+        if(valid):
+            count += 1
+        await ClockCycles(dut.clk, 1)
+    count =0
+    gold_out = {}
+    rtl_out = {}
+    for band in bands:
+        band_dout = {str(band):[]}
+        gold_out.update(band_dout)
+        band_dout = {str(band):[]}
+        rtl_out.update(band_dout)
+    write_count = 0
+    while(write_count < iters): 
+        valid = int(dut.dout_valid.value)
+        error = int(dut.dout_error.value)
+        if(error):
+            count += 1
+            pass
+        band_out = int(dut.band_out.value)
+        if(valid):
+            e1 = int(dut.eigen1_y.value)
+            ex = int(dut.eigen_x.value)
+            outs = np.array([e1,ex])
+            outs = two_comp_unpack(outs, dout_width, dout_pt)
+            gold_phase = np.rad2deg(np.arctan2(gold[2][count].real, gold[4][count].real)*2)
+            rtl_phase = np.rad2deg(np.arctan2(float(outs[0]), float(outs[1]))*2)
+            if(np.isin((band_out), bands).any()):
+                gold_out[str(band_out)].append(gold_phase)
+                rtl_out[str(band_out)].append(rtl_phase)
+                write_count +=1 
+            count +=1
+        await ClockCycles(dut.clk, 1)
+    return [gold_out, rtl_out]
 
 
 ###
@@ -291,10 +388,9 @@ async def check_correlator_output(dut, gold, vec_len, dout_width, dout_pt, thres
             assert (np.abs(r11-gold[0][count])<thresh), "Error R11"
             assert (np.abs(r22-gold[1][count])<thresh), "Error R22"
             assert (np.abs(r12-gold[2][count].real)<thresh), "Error R12_re"
-
             count += 1
         await ClockCycles(dut.clk, 1)
-
+    return collect
         
 async def check_sqrt_output(dut, gold, vec_len, dout_width, dout_pt, thresh):
     #TODO
